557b93e206c37a48737846726f94dbad7a747b49
admin <admin@Ohene-Mills.local>
2023-08-16 10:29:55 +0000

    new

--- data.js
+++ data.js
@@ -2,56 +2,56 @@ let songs = [
     
     {
         name: 'Bravazos',
-        path: '/musics/Song 3.mp3',
+        path: './musics/Song 3.mp3',
         artist: 'Ep2 Students from South Africa',
-        cover: '/images/radio2.jpg'
+        cover: './images/radio2.jpg'
     },
     {
         name: 'Bravazo',
-        path: '/musics/radio2.mp3',
+        path: './musics/radio2.mp3',
         artist: 'Ep3 Students from South Africa',
-        cover: '/images/radio2.jpg'
+        cover: './images/radio2.jpg'
     },
     {
         name: 'song 2',
-        path: '/musics/Song 2.mp3',
+        path: './musics/Song 2.mp3',
         artist: 'artist 2',
-        cover: '/images/cover 2.png'
+        cover: './images/cover 2.png'
     },
     {
         name: 'song 3',
-        path: '/musics/Song 3.mp3',
+        path: './musics/Song 3.mp3',
         artist: 'artist 3',
-        cover: '/images/cover 3.png'
+        cover: './images/cover 3.png'
     },
     {
         name: 'song 4',
-        path: '/musics/Song 4.mp3',
+        path: './musics/Song 4.mp3',
         artist: 'artist 4',
-        cover: '/images/cover 4.png'
+        cover: './images/cover 4.png'
     },
     {
         name: 'song 5',
-        path: '/musics/Song 5.mp3',
+        path: './musics/Song 5.mp3',
         artist: 'artist 5',
-        cover: '/images/cover 5.png'
+        cover: './images/cover 5.png'
     },
     {
         name: 'song 6',
-        path: '/musics/Song 6.mp3',
+        path: './musics/Song 6.mp3',
         artist: 'artist 6',
-        cover: '/images/cover 6.png'
+        cover: './images/cover 6.png'
     },
     {
         name: 'song 7',
-        path: '/musics/Song 7.mp3',
+        path: './musics/Song 7.mp3',
         artist: 'artist 7',
-        cover: '/images/cover 7.png'
+        cover: './images/cover 7.png'
     },
     {
         name: 'song 8',
-        path: '/musics/Song 8.mp3',
+        path: './musics/Song 8.mp3',
         artist: 'artist 8',
-        cover: '/images/cover 8.png'
+        cover: './images/cover 8.png'
     },
 ]
